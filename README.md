f448a3d2e8f1d6e9e1e980017294efb1569f0509
Stanislav Pikalov <stanislav.pikalov@domrf.ru>
2023-01-05 11:31:34 +0300

    basic auth

---
+++ README.md
@@ -0,0 +1,14 @@
+# Startup
+You are required to create a secured restful API to create and list articles
+- To create an article the user should provide a title, author, the content and date for publishing.
+- All the fields are mandatory and the title should not exceed 100 characters. The publishing date should bind to ISO 8601 format.
+- Article results should be paginated.
+
+Additionally, you are required to develop an endpoint for statistics that would be accessible only by admins.
+- The endpoint should return count of published articles on daily bases for the 7 days
+
+Please also write tests to verify that your code works.
+
+### Basic auth
+1) user:password
+2) admin:password
--- src/main/java/com/startup/startup/config/SecurityConfig.java
+++ src/main/java/com/startup/startup/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.startup.startup.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,17 +11,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}password")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("{noop}password")
+                .roles("ADMIN", "USER");
     }
 
     @Override
-    public void configure(WebSecurity web) {
-        web
-                .ignoring()
-                .antMatchers("/h2-console/**", "/**");
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/article").hasRole("USER")
+                .antMatchers("/article/stats").hasRole("ADMIN")
+                .and()
+                .httpBasic();
     }
 }
 
